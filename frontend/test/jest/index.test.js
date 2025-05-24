e2ceb1447e0f74952f7b27cee24166c51c60235e
Nick Müllner <if23b503@technikum-wien.at>
2025-05-24 17:43:52 +0200

    Making k6 and jest work and added snyk

--- frontend/test/jest/index.test.js
+++ frontend/test/jest/index.test.js
@@ -1,4 +1,4 @@
-import { expect, test } from "@jest/globals";
+const { expect, test } = require("@jest/globals");
 
 test("adds 1 + 2 to equal 3", () => {
   expect(1 + 2).toBe(3);
--- frontend/test/k6/load-test.js
+++ frontend/test/k6/load-test.js
@@ -3,14 +3,14 @@ import { check, sleep } from "k6";
 
 export let options = {
   stages: [
-    { duration: "30s", target: 20 }, // 20 User innerhalb 30 Sek.
-    { duration: "1m", target: 100 }, // Peak: 100 User für 1 Min.
-    { duration: "30s", target: 0 }, // Cooldown
+    { duration: "5s", target: 20 }, // 20 User innerhalb 30 Sek.
+    { duration: "5s", target: 100 }, // Peak: 100 User für 1 Min.
+    { duration: "5s", target: 0 }, // Cooldown
   ],
 };
 
 export default function () {
-  const res = http.get("https://staging.secret-notes.example.com/");
+  const res = http.get("http://localhost:8080/");
   check(res, {
     "status is 200": (r) => r.status === 200,
     "body contains title": (r) => r.body.includes("Secret Notes"),
