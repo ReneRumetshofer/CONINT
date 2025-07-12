3f06b8d2fe7a6573dd3ae59fee3062ea004a167f
Nick Müllner <collina2003@scouts.at>
2025-07-12 22:35:10 +0200

    Updated stack

--- frontend/playwright.config.js
+++ frontend/playwright.config.js
@@ -1,5 +1,12 @@
 const { defineConfig } = require("@playwright/test");
 
 module.exports = defineConfig({
+  use: {
+    browserName: "chromium",
+    headless: true,
+    launchOptions: {
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    },
+  },
   testDir: "./test/e2e",
 });
